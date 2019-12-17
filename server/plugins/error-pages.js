8a5b1b6b66044a3edfd482c7796083ec42bfd48d
ben <ben@surgison.net>
2019-12-17 09:07:00 +0000

    Integrate flow engine

--- server/plugins/error-pages.js
+++ server/plugins/error-pages.js
@@ -1,13 +1,13 @@
-const { errorPages, routeFlow } = require('defra-hapi-modules').plugins
-let flow
+const { errorPages } = require('defra-hapi-modules').plugins
 
 module.exports = {
   plugin: errorPages,
   options: {
-    handleFailedPrecondition: (request, h) => {
+    handleFailedPrecondition: async (request, h) => {
+      const { flow } = request.server.app
       // Just redirect home for now
-      flow = flow || routeFlow.flow()
-      return h.redirect(flow.home.path)
+      const route = await flow('home')
+      return h.redirect(route.path)
     }
   }
 }
