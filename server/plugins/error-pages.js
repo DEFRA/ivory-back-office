0f2537c6c8a376b9736df43b6b2b34e78dd6ffbb
ben <ben@surgison.net>
2019-12-18 12:16:06 +0000

    Create error pages plugin

--- server/plugins/error-pages.js
+++ server/plugins/error-pages.js
@@ -1,13 +1,12 @@
-const { errorPages } = require('defra-hapi-modules').plugins
-
 module.exports = {
-  plugin: errorPages,
+  plugin: require('defra-hapi-error-handling'),
   options: {
     handleFailedPrecondition: async (request, h) => {
       const { flow } = request.server.app
       // Just redirect home for now
       const route = await flow('home')
       return h.redirect(route.path)
-    }
+    },
+    errorViewLocation: 'errors'
   }
 }
