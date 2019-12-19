1f9ff445bcd0873334a9893380e9414893104188
ben <ben@surgison.net>
2019-12-19 15:05:48 +0000

    APHA registrations

--- server/modules/registrations/registrations.handlers.js
+++ server/modules/registrations/registrations.handlers.js
@@ -1,10 +1,51 @@
-// const { Registration } = require('ivory-data-mapping').cache
+const { Persistence } = require('defra-hapi-utils')
+const config = require('../../config')
+const moment = require('moment')
 
 class RegistrationsHandlers extends require('defra-hapi-handlers') {
+  async load (path) {
+    return Persistence.createDAO({ path: `${config.serviceApi}${path}` }).restore()
+  }
+
   // Overrides parent class handleGet
   async handleGet (request, h, errors) {
-    // const registrations = await Registration.getAll(request) || []
-    // this.viewData = { registrations }
+    const registrations = await this.load('/full-registrations?status=submitted') || []
+    const referenceData = await this.load('/reference-data')
+
+    const itemTypes = {}
+    referenceData.itemType.choices.forEach(({ shortName, display }) => {
+      itemTypes[shortName] = display
+    })
+
+    const head = [
+      { text: 'Photo / registration number' },
+      { text: 'Item type' },
+      { text: 'Description' },
+      { text: 'Submitted' }
+    ]
+
+    const rows = registrations.map((registration) => {
+      const { registrationNumber, item, submittedDate } = registration
+      const { itemType, description, photos } = item
+      const { filename } = photos[0]
+
+      return [
+        {
+          html: `
+            <a href="item-detail/${registrationNumber}">
+                <img src="/photos/small/${filename}" alt="${description}" class="item-list-thumbnail" />
+                <br />
+                ${registrationNumber}
+            </a>
+`
+        },
+        { text: itemTypes[itemType] },
+        { text: description },
+        { text: moment(submittedDate).format('YYYY-MM-DD HH:ss') }
+      ]
+    })
+
+    this.viewData = { head, rows }
     return super.handleGet(request, h, errors)
   }
 }
--- server/modules/registrations/registrations.route.test.js
+++ server/modules/registrations/registrations.route.test.js
@@ -1,11 +1,34 @@
 const Lab = require('@hapi/lab')
 const lab = exports.lab = Lab.script()
 const TestHelper = require('../../../test-helper')
+const { Persistence } = require('defra-hapi-utils')
+const config = require('../../config')
 const url = '/registrations'
-const pageHeading = 'Registrations'
+const pageHeading = 'Ivory registrations'
+const serviceApi = 'http://ivory-test-api.com'
 
 lab.experiment(TestHelper.getFile(__filename), () => {
   const routesHelper = TestHelper.createRoutesHelper(lab, __filename)
 
-  routesHelper.getRequestTests({ lab, pageHeading, url })
+  routesHelper.getRequestTests({ lab, pageHeading, url }, () => {
+    lab.beforeEach(({ context }) => {
+      const { sandbox } = context
+
+      const registrations = []
+      const referenceData = {
+        itemType: {
+          choices: []
+        }
+      }
+
+      sandbox.stub(config, 'serviceApi').value(serviceApi)
+      sandbox.stub(Persistence, 'createDAO').value(({ path }) => {
+        switch (path) {
+          case `${serviceApi}/full-registrations?status=submitted`: return { restore: () => registrations }
+          case `${serviceApi}/reference-data`: return { restore: () => referenceData }
+          default: throw new Error(`Unexpected persistence path: ${path}`)
+        }
+      })
+    })
+  })
 })
