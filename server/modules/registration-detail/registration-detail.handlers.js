f967c4b3b1096af6995e3b3b5bb72fefa94d51ae
ben <ben@surgison.net>
2019-12-24 12:54:38 +0000

    Registration details part 2

--- server/modules/registration-detail/registration-detail.handlers.js
+++ server/modules/registration-detail/registration-detail.handlers.js
@@ -1,5 +1,6 @@
 const { Persistence } = require('defra-hapi-utils')
 const config = require('../../config')
+const moment = require('moment')
 
 class RegistrationDetailHandlers extends require('defra-hapi-handlers') {
   async load (path) {
@@ -7,34 +8,87 @@ class RegistrationDetailHandlers extends require('defra-hapi-handlers') {
   }
 
   async getRegistration (request) {
-    const result = await this.load(`/full-registrations?registrationNumber=${request.params.registrationNumber}`) || []
-    return result[0]
+    // Use request app object to cache the registration in this request
+    if (!request.app.registration) {
+      const result = await this.load(`/full-registrations?registrationNumber=${request.params.registrationNumber}`) || []
+      request.app.registration = result[0]
+    }
+    return request.app.registration
   }
 
-  async getItemType (type) {
-    const { itemType } = await this.load('/reference-data')
-    const { display } = itemType.choices.find(({ shortName }) => shortName === type)
-    return display
+  async getItemType (request, type) {
+    // Use request app object to cache itemTypes in this request
+    if (!request.app.itemTypes) {
+      const { itemType } = await this.load('/reference-data')
+      request.app.itemTypes = itemType.choices
+    }
+    return request.app.itemTypes.find(({ shortName }) => shortName === type)
   }
 
   async getPageHeading (request) {
     const { registrationNumber, item } = await this.getRegistration(request) || {}
-    const itemType = await this.getItemType(item.itemType)
-    return `${registrationNumber} - ${itemType}`
+    const { display } = await this.getItemType(request, item.itemType)
+    return `${registrationNumber} - ${display}`
+  }
+
+  async getNodeHeading (node, request) {
+    // Abstract this away to allow ease of testing
+    return node.handlers.getPageHeading(request)
   }
 
   async getBreadcrumbs (request) {
-    const { handlers, path } = await this.getFlowNode('registrations')
+    const registrationNode = await this.getFlowNode('registrations')
     return [{
-      text: await handlers.getPageHeading(request),
-      href: path
+      text: await this.getNodeHeading(registrationNode, request),
+      href: registrationNode.path
     }, {
       text: await this.getPageHeading(request)
     }]
   }
 
+  buildRows (details) {
+    return Object.entries(details)
+      // Ignore all details that are not entered
+      .filter(([_, detail]) => detail)
+      // Map to summary list format
+      .map(([heading, detail]) => {
+        return {
+          key: { text: heading },
+          value: { text: detail }
+        }
+      })
+  }
+
   // Overrides parent class handleGet
   async handleGet (request, h, errors) {
+    const { item = {}, agent = {}, owner = {}, submittedDate = '' } = await this.getRegistration(request) || {}
+    const { description, ageExemptionDescription, volumeExemptionDescription, photos = [] } = item
+    const { ageExemptionLabel = '', volumeExemptionLabel = '' } = await this.getItemType(request, item.itemType)
+    const { filename } = photos.pop() || {}
+
+    const details = {
+      Description: description,
+      [`${ageExemptionLabel} explanation`]: ageExemptionDescription,
+      [`${volumeExemptionLabel} explanation`]: volumeExemptionDescription,
+      Agent: agent.fullName,
+      'Agent\'s email': agent.email,
+      'Agent\'s address': agent.address,
+      Owner: owner.fullName,
+      'Owner\'s email': owner.email,
+      'Owner\'s address': owner.address,
+      Submitted: moment(submittedDate).format('YYYY-MM-DD HH:mm')
+    }
+
+    this.viewData = {
+      rows: [
+        {
+          key: {
+            html: `<img class="item-detail-main govuk-!-margin-bottom-5" src="/photos/small/${filename}" alt="${description}">`
+          }
+        },
+        ...this.buildRows(details)
+      ]
+    }
     return super.handleGet(request, h, errors)
   }
 }
